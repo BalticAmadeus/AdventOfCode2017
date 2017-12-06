3c88cc8d2fa2efcf36c90a25e181ce070447befb
Edvinas Sulcas <edvinas.sulcas@gmail.com>
2017-12-06 07:28:24 +0000

    tarpinė versija, online runner neužtenka memory :D

---
+++ day6/edvinas/advent6_1.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+					
+public class Program
+{
+	public static List<int> bankList;
+	
+	public static void Main()
+	{
+		string input = "0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11";
+		//string input = "0 2 7 0";
+		
+		bankList = input.Split().Select(int.Parse).ToList();
+		int cycles = 0;
+		int maxIndex;
+
+		List<string> snapshots = new List<string>();
+		snapshots.Add(String.Join(", ", bankList.ToArray()));
+		
+		while (true)
+		{
+			cycles++;
+			maxIndex = bankList.IndexOf(bankList.Max());
+			redistribute(maxIndex);
+			
+			Console.WriteLine(String.Join(", ", bankList.ToArray()));
+			if (snapshots.Contains(String.Join(", ", bankList.ToArray())))
+				break;
+			snapshots.Add(String.Join(", ", bankList.ToArray()));
+		}
+
+		
+		Console.WriteLine(cycles);
+	}
+	
+	public static void redistribute(int bankNumber)
+	{
+		int currentBank = bankNumber;
+		int numBlocks = bankList[bankNumber];
+		
+		bankList[bankNumber] = 0;
+		
+		for (int i = numBlocks; i > 0; i--)
+		{
+			currentBank++;
+			if (currentBank >= bankList.Count())
+				currentBank = 0;
+			
+			bankList[currentBank]++;
+		}
+	}
+}
