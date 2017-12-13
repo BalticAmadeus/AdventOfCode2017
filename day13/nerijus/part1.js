941ed4f24ba6476e2100503c45ddc0cfb10428ac
fobas <nerijusv.lt@gmail.com>
2017-12-13 20:49:07 +0200

    Fix part 1 of day 13

--- day13/nerijus/part1.js
+++ day13/nerijus/part1.js
@@ -12,10 +12,22 @@ var input = fs.readFileSync('input.dat', 'utf-8').split('\n').map(val => {
 
 var score = 0;
 
-
 for (var i=0; i < firewalls.length; i++){
+    console.log(firewalls[4]);
     if (firewalls[i] && firewalls[i].position == 0){
         score += i * firewalls[i].range;
+    }
+
+    for (var j=0; j < firewalls.length; j++){
+        if (firewalls[j]){
+            if (firewalls[j].dir == "D"){
+                firewalls[j].dir = firewalls[j].range == firewalls[j].position + 1 ? "U" : "D";
+                firewalls[j].position += firewalls[j].range == firewalls[j].position + 1 ? -1 : 1;
+            } else {
+                firewalls[j].dir = firewalls[j].position == 0 ? "D" : "U";
+                firewalls[j].position += firewalls[j].position == 0 ? 1 : -1;
+            }
+        }
     }
 }
 
