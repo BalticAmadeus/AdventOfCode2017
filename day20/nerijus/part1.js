11382de1d335224bea43c63d48040c4ee57a245d
fobas <nerijusv.lt@gmail.com>
2017-12-20 08:05:32 +0200

    Just wait a second

---
+++ day20/nerijus/part1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+var particles = fs.readFileSync('input.dat', 'utf-8').split('\n').map(x => {
+    //p=<-520,2504,414>, v=<-74,354,60>, a=<-2,-25,-7>
+    x = x.split(', ');
+
+    return {
+        p: x[0].substr(3,x[0].length-4).split(",").map(y=>Number(y)),
+        v: x[1].substr(3,x[1].length-4).split(",").map(y=>Number(y)),
+        a: x[2].substr(3,x[2].length-4).split(",").map(y=>Number(y))
+    }
+});
+
+console.log(particles);
+
+var part = null;
+
+
+for (let i=0; i < 10000000; i++){
+    let minPart = null;
+    let min = 999999999999999;
+    
+    for (let j=0; j < particles.length; j++){
+        let p = particles[j];
+
+        if (Math.abs(p.p[0]) + Math.abs(p.p[1]) + Math.abs(p.p[2]) < min){
+            min = Math.abs(p.p[0]) + Math.abs(p.p[1]) + Math.abs(p.p[2]);
+
+            minPart = j;
+        }
+
+        p.v[0] += p.a[0];
+        p.v[1] += p.a[1];
+        p.v[2] += p.a[2];
+
+        p.p[0] += p.v[0];
+        p.p[1] += p.v[1];
+        p.p[2] += p.v[2];
+    }
+
+    console.log(min, minPart);
+}
+
+console.log(min);
